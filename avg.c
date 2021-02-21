5c35dfaff35d467c3c91964d4fac0b4df5da6e0f
vishalgupta95 <77653464+vishalgupta95@users.noreply.github.com>
2021-02-20 20:18:49 -0500

    Add files via upload

--- avg.c
+++ avg.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 
-#if 0
-#include <iostream>
-#include <algorithm>
-#endif
-
-void disp(int *ptr, int count)
+void display(int *ptr, int count)
 {
     int i = 0;
-    //sort(ptr, ptr + count);
-    //std::unique(ptr, ptr + count);  // remove duplicate,c++ library
-    //sort(ptr, ptr + count, greater<int>()); // reverse,c++ library
 
     for (i = 0; i < count; i++)
     {
@@ -20,46 +12,50 @@ void disp(int *ptr, int count)
     printf("\n\n");
 }
 
-double getAverage1(int x[5],int count)
+double getAverage1(int x[5], int count)
 {
-	int i=0;double j;
-	for(i=0;i<count;i++){
-		j += x[i];
+    int i = 0;
+    double j;
+    for (i = 0; i < count; i++)
+    {
+        j += x[i];
     }
-	return(j/count);
+    return (j / count);
 }
 
-
-double getAverage2(int *x,int count)
+double getAverage2(int *x, int count)
 {
-	int i=0;double j;
-	for(i=0;i<count;i++){
-		j += x[i];
+    int i = 0;
+    double j;
+    for (i = 0; i < count; i++)
+    {
+        j += x[i];
     }
-	return(j/count);
+    return (j / count);
 }
 
-int main ()
+int main()
 {
-   /* an int array with 5 elements */
-   int balance[5] = {1000, 2, 3, 17, 50};
-   double avg;
-   int x = sizeof(balance)/sizeof(balance[0]);
-   printf("val =%d \n",x);
-   /* pass pointer to the array as an argument */
-   disp(balance);
-   avg = getAverage( balance, x ) ;
-   printf( "Average value is: %f  \n", avg );
-   
-   disp(balance);
-   avg = getAverage1( balance, x ) ;
-   printf( "Average value is: %f \n ", avg );
-   disp(balance);
-  
-   avg = getAverage2( balance, x ) ;
-   printf( "Average value is: %f \n", avg );
-   disp(balance, x);
-   disp(balance);
-    
-   return 0;
+
+    /* an int array with 5 elements */
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+    int count = sizeof(balance) / sizeof(balance[0]);
+    printf("count =%d \n", count);
+
+    /* pass pointer to the array as an argument */
+    display(balance, count);
+
+    avg = getAverage1(balance, count);
+    printf("Average value is: %f  \n", avg);
+
+    display(balance, count);
+    avg = getAverage1(balance, count);
+    printf("Average value is: %f \n ", avg);
+    display(balance, count);
+
+    avg = getAverage2(balance, count);
+    printf("Average value is: %f \n", avg);
+    display(balance, count);
+    return 0;
 }

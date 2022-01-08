4e7d60c0c34ae5d8a4df5c0411fad5d8c024c49d
vishal gupta <vishagu2@cisco.com>
2022-01-08 12:33:27 -0800

    brief

---
+++ anagram.cpp
@@ -0,0 +1,145 @@
+
+/* Method 1*/
+
+#include <stdio.h>
+#define NO_OF_CHARS 256
+
+/* function to check whether two strings are anagram of
+each other */
+bool areAnagram(char* str1, char* str2)
+{
+    // Create 2 count arrays and initialize all values as 0
+    int count1[NO_OF_CHARS] = { 0 };
+    int count2[NO_OF_CHARS] = { 0 };
+    int i;
+
+    // For each character in input strings, increment count
+    // in the corresponding count array
+    for (i = 0; str1[i] && str2[i]; i++) {
+        count1[str1[i]]++;
+        count2[str2[i]]++;
+    }
+
+    // If both strings are of different length. Removing
+    // this condition will make the program fail for strings
+    // like "aaca" and "aca"
+    if (str1[i] || str2[i])
+        return false;
+
+    // Compare count arrays
+    for (i = 0; i < NO_OF_CHARS; i++)
+        if (count1[i] != count2[i])
+            return false;
+
+    return true;
+}
+
+/* Driver code*/
+int main()
+{
+    char str1[] = "geeksforgeeks";
+    char str2[] = "forgeeksgeeks";
+
+    // Function Call
+    if (areAnagram(str1, str2))
+        printf("The two strings are anagram of each other");
+    else
+        printf("The two strings are not anagram of each "
+            "other");
+
+    return 0;
+}
+
+/* Method 2*/
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+/* function to check whether two strings are anagram of
+each other */
+bool areAnagram(string str1, string str2)
+{
+    // Get lengths of both strings
+    int n1 = str1.length();
+    int n2 = str2.length();
+
+    // If length of both strings is not same, then they
+    // cannot be anagram
+    if (n1 != n2)
+        return false;
+
+    // Sort both the strings
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+    // Compare sorted strings
+    for (int i = 0; i < n1; i++)
+        if (str1[i] != str2[i])
+            return false;
+
+    return true;
+}
+
+// Driver code
+int main()
+{
+    string str1 = "test";
+    string str2 = "ttew";
+
+    // Function Call
+    if (areAnagram(str1, str2))
+        cout << "The two strings are anagram of each other";
+    else
+        cout << "The two strings are not anagram of each "
+                "other";
+
+    return 0;
+}
+
+/* Method 3*/
+#include <stdio.h>
+int check_anagram(char [], char []);
+
+int main()
+{
+  char a[1000], b[1000];
+
+  printf("Enter two strings\n");
+  gets(a);
+  gets(b);
+
+  if (check_anagram(a, b))
+    printf("The strings are anagrams.\n");
+  else
+    printf("The strings aren't anagrams.\n");
+
+  return 0;
+}
+
+int check_anagram(char a[], char b[])
+{
+  int first[26] = {0}, second[26] = {0}, c=0;
+
+  // Calculating frequency of characters of the first string
+
+  while (a[c] != '\0') {
+    first[a[c]-'a']++;
+    c++;
+  }
+
+  c = 0;
+
+  while (b[c] != '\0') {
+    second[b[c]-'a']++;
+    c++;
+  }
+
+  // Comparing the frequency of characters
+
+  for (c = 0; c < 26; c++)
+    if (first[c] != second[c])
+      return 0;
+
+  return 1;
+}

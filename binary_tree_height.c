7ba68b8eab526f134315df7d0c7437471a8cce12
vishal gupta <vishagu2@cisco.com>
2022-01-08 15:08:10 -0800

    brief

--- binary_tree_height.c
+++ binary_tree_height.c
@@ -2,19 +2,29 @@
 #include <stdlib.h>
 
 /* The height of a binary tree is the largest number of edges in a path from the root node to a leaf node.
-   edge means line joing 2 nodes  , so binary tree with 4 nodes on eitherside will have 3 edges*/
+   edge means line joing 2 nodes  , so binary tree with 4 nodes on eitherside will have 3 edges
 
+   The level of a node is the number of edges along the unique path between it and the root node. 
+   Therefore, the root node has a level of 0. If it has children, both of them have a level of 
+          10--- > level 0
+      20          19   level 1
+   5
+1      6                level 3    ( height is 3
+          
+*/
 //
-//For example, left skewed binary tree shown in Figure 1(a) with 5 nodes has height 5-1 = 4 and binary tree shown in Figure 1(b) 
+//For example, left skewed binary tree shown   with 5 nodes has height 5-1 = 4 and binary tree shown in Figure 1(b) 
 //with 5 nodes has height floor(log25) = 2.
 
-/* A binary tree node has data, pointer to left child
-and a pointer to right child */
+/* A binary tree node has data, pointer to left child and a pointer to right child */
+
 struct node {
 	int data;
 	struct node* left;
 	struct node* right;
 };
+
+/* Method 1 : recursion ,Time Complexity: O(n)  */
 
 /* Compute the "maxDepth" of a tree -- the number of
 	nodes along the longest path from the root node
@@ -22,7 +32,7 @@ struct node {
 int maxDepth(struct node* node)
 {
 	if (node == NULL)
-		return 0;
+	     return -1;
 	else {
 		/* compute the depth of each subtree */
 		int lDepth = maxDepth(node->left);
@@ -52,14 +62,22 @@ struct node* newNode(int data)
 int main()
 {
 	struct node* root = newNode(1);
-
 	root->left = newNode(2);
 	root->right = newNode(3);
 	root->left->left = newNode(4);
 	root->left->right = newNode(5);
 
-	printf("Height of tree is %d", maxDepth(root));
+	printf("Height of regular  binary tree with max 3 levels is %d \n", maxDepth(root));
 
-	getchar();
+  
+	struct node* root1 = newNode(1);
+	root1->left = newNode(2);
+	root1->left->left = newNode(3);
+	root1->left->left->left = newNode(4);
+	root1->left->left->left->left = newNode(5);
+	root1->left->left->left->left->left = newNode(6);
+	printf("Height of skewed binary tree with 6 nodes from level 0 root to level 5 is %d \n", maxDepth(root1));
+
+
 	return 0;
 }

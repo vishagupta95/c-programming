992a7c627ff54ce49b6c7b2e94bec302779feb18
vishal gupta <vishagu2@cisco.com>
2022-02-06 20:39:59 -0800

    brief

--- quicksort.c
+++ quicksort.c
@@ -1,4 +1,19 @@
 /* C implementation QuickSort */
+/*
+Algorithm Time Complexities Best Average Worst
+Selection Sort    Ω(n^2)                 θ(n^2)                      O(n^2)
+Bubble Sort       Ω(n)                   θ(n^2)                      O(n^2)
+Insertion Sort    Ω(n)                   θ(n^2)                      O(n^2)
+Heap Sort         Ω(n log(n))            θ(n log(n))                 O(n log(n))
+Quick Sort        Ω(n log(n))            θ(n log(n))                 O(n^2)
+Merge Sort        Ω(n log(n))            θ(n log(n))                 O(n log(n))
+Bucket Sort       Ω(n+k)                 θ(n+k)                      O(n^2) 
+Radix Sort        Ω(nk)                  θ(nk)                       O(nk)
+
+To summarise the two examples above, O(1) denotes constant space use: the algorithm allocates the same number of pointers irrespective to the list size. 
+In contrast, O(N) denotes linear space use: the algorithm space use grows together with respect to the input size.
+*/
+
 #include<stdio.h> 
 
 // A utility function to swap two elements 

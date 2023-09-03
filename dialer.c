32d48920dd28476e7049f6a89c85122e0bae8490
vishalgupta95 <77653464+vishalgupta95@users.noreply.github.com>
2023-09-02 22:40:41 -0400

    Update dialer.c

--- dialer.c
+++ dialer.c
@@ -30,39 +30,48 @@ int main()
 
 */
 
-char *solution(char *A[], char *B[], int N, char *P)
-{
-    static char array[1024] ={};
-    char *dest = &array[0];
-    memset(dest, 0, sizeof(2048));
-    int no_match = 1;
+#include <stdio.h>
+#include <string.h>
 
-    for (int i = 0; i < N; i++)
-    {
-        if (strstr(B[i], P))
-        {
-            strcat(dest, A[i]);
-            strcat(dest, " ");
-            no_match = 0;
+// Structure to store phone book entries
+struct PhoneEntry {
+    char name[50];
+    char phoneNumber[15]; // Assuming phone numbers are up to 15 characters long
+};
+
+// Function to search for a phone number by name
+const char* findPhoneNumber(const char* name, struct PhoneEntry phoneBook[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (strstr(phoneBook[i].name, name) != NULL) {
+            return phoneBook[i].phoneNumber;
         }
     }
+    return "Not Found"; // Return this if the name is not found
+}
 
-    if (no_match == 1)
-    {
-        strcpy(dest, "NO CONTACT");
+int main() {
+    // Create a simple phone book with a few entries
+    struct PhoneEntry phoneBook[] = {
+        {"Alice", "123-456-7890"},
+        {"Bob", "456-789-1230"},
+        {"Charlie", "789-123-4560"},
+    };
+
+    int size = sizeof(phoneBook) / sizeof(phoneBook[0]);
+
+    // Prompt the user for a name and look up the phone number
+    char searchName[50];
+    printf("Enter a name to find the associated phone number: ");
+    scanf("%s", searchName);
+
+    const char* phoneNumber = findPhoneNumber(searchName, phoneBook, size);
+
+    if (strcmp(phoneNumber, "Not Found") == 0) {
+        printf("Name not found in the phone book.\n");
+    } else {
+        printf("Phone number for %s: %s\n", searchName, phoneNumber);
     }
 
-    return dest;
-}
-
-int main()
-{
-    char *name[] = {"visha", "vishal", "vishal gupta", "vishalgupta95"};
-    char *name2[] = {"rishi", "vishal", "vishal gupta", "vishalgupta957"};
-    char *phone[] = {"9886983955", "9886983950", "9886983951", "9886983952"};
-    int argc = sizeof(name) / sizeof(name[0]);
-    printf("argc = %d\n ", argc);
-    printf("%s \n",solution(name, phone, argc, "9886"));
-    printf("%s \n",solution(name2, phone, argc, "9886"));
     return 0;
 }
+
